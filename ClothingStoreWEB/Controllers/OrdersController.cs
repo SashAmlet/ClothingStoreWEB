333d8ba3d4553f1f9403d0cac918ee7a1b31256a
Sasha Ostrenko <ostrenkosasha@gmail.com>
2023-09-08 10:20:52 +0300

    All the default Controllers-Views (CRUD) are done

---
+++ ClothingStoreWEB/Controllers/OrdersController.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ClothingStore.Context;
+using ClothingStore.Models;
+
+namespace ClothingStoreWEB.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly MainContext _context;
+
+        public OrdersController(MainContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Orders
+        public async Task<IActionResult> Index()
+        {
+            var mainContext = _context.Orders.Include(o => o.Delivery).Include(o => o.Payment).Include(o => o.Status).Include(o => o.User);
+            return View(await mainContext.ToListAsync());
+        }
+
+        // GET: Orders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.Delivery)
+                .Include(o => o.Payment)
+                .Include(o => o.Status)
+                .Include(o => o.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // GET: Orders/Create
+        public IActionResult Create()
+        {
+            ViewData["DeliveryId"] = new SelectList(_context.DeliveryMethods, "Id", "Id");
+            ViewData["PaymentId"] = new SelectList(_context.PaymentMethods, "Id", "Id");
+            ViewData["StatusId"] = new SelectList(_context.Statuses, "Id", "Id");
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Orders/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,StatusId,PaymentId,DeliveryId,Comment")] Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(order);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DeliveryId"] = new SelectList(_context.DeliveryMethods, "Id", "Id", order.DeliveryId);
+            ViewData["PaymentId"] = new SelectList(_context.PaymentMethods, "Id", "Id", order.PaymentId);
+            ViewData["StatusId"] = new SelectList(_context.Statuses, "Id", "Id", order.StatusId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", order.UserId);
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            ViewData["DeliveryId"] = new SelectList(_context.DeliveryMethods, "Id", "Id", order.DeliveryId);
+            ViewData["PaymentId"] = new SelectList(_context.PaymentMethods, "Id", "Id", order.PaymentId);
+            ViewData["StatusId"] = new SelectList(_context.Statuses, "Id", "Id", order.StatusId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", order.UserId);
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,StatusId,PaymentId,DeliveryId,Comment")] Order order)
+        {
+            if (id != order.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DeliveryId"] = new SelectList(_context.DeliveryMethods, "Id", "Id", order.DeliveryId);
+            ViewData["PaymentId"] = new SelectList(_context.PaymentMethods, "Id", "Id", order.PaymentId);
+            ViewData["StatusId"] = new SelectList(_context.Statuses, "Id", "Id", order.StatusId);
+            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", order.UserId);
+            return View(order);
+        }
+
+        // GET: Orders/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Orders == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.Delivery)
+                .Include(o => o.Payment)
+                .Include(o => o.Status)
+                .Include(o => o.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Orders == null)
+            {
+                return Problem("Entity set 'MainContext.Orders'  is null.");
+            }
+            var order = await _context.Orders.FindAsync(id);
+            if (order != null)
+            {
+                _context.Orders.Remove(order);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderExists(int id)
+        {
+          return (_context.Orders?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ ClothingStoreWEB/Controllers/PaymentsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ClothingStore.Context;
+using ClothingStore.Models.AuxiliaryModels;
+
+namespace ClothingStoreWEB.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        private readonly MainContext _context;
+
+        public PaymentsController(MainContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Payments
+        public async Task<IActionResult> Index()
+        {
+              return _context.PaymentMethods != null ? 
+                          View(await _context.PaymentMethods.ToListAsync()) :
+                          Problem("Entity set 'MainContext.PaymentMethods'  is null.");
+        }
+
+        // GET: Payments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.PaymentMethods == null)
+            {
+                return NotFound();
+            }
+
+            var payment = await _context.PaymentMethods
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+
+            return View(payment);
+        }
+
+        // GET: Payments/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Payments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(payment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(payment);
+        }
+
+        // GET: Payments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.PaymentMethods == null)
+            {
+                return NotFound();
+            }
+
+            var payment = await _context.PaymentMethods.FindAsync(id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+            return View(payment);
+        }
+
+        // POST: Payments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Payment payment)
+        {
+            if (id != payment.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(payment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PaymentExists(payment.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(payment);
+        }
+
+        // GET: Payments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.PaymentMethods == null)
+            {
+                return NotFound();
+            }
+
+            var payment = await _context.PaymentMethods
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (payment == null)
+            {
+                return NotFound();
+            }
+
+            return View(payment);
+        }
+
+        // POST: Payments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.PaymentMethods == null)
+            {
+                return Problem("Entity set 'MainContext.PaymentMethods'  is null.");
+            }
+            var payment = await _context.PaymentMethods.FindAsync(id);
+            if (payment != null)
+            {
+                _context.PaymentMethods.Remove(payment);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PaymentExists(int id)
+        {
+          return (_context.PaymentMethods?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
