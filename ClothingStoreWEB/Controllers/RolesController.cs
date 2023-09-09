22dec443a26c9279f40e544d1f993fee72075cab
Sasha Ostrenko <ostrenkosasha@gmail.com>
2023-09-09 18:17:10 +0300

    loging with google(first try)

--- ClothingStoreWEB/Controllers/RolesController.cs
+++ ClothingStoreWEB/Controllers/RolesController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ClothingStoreWEB.Controllers
 {
-/*    [Authorize(Roles = "admin")]*/
+    [Authorize(Roles = "admin")]
     public class RolesController : Controller
     {
         RoleManager<IdentityRole> _roleManager;
